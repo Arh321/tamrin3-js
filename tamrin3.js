f6234605b9c098d37e36f9c1b335d9ea502c10d4
Alireza Hasanzade <alirezawtf321@gmail.com>
2023-05-08 08:26:29 -0700

    tamren 3 js

---
+++ tamrin3.js
@@ -0,0 +1,20 @@
+const text = "DDDeshkar is the best";
+
+let textArray = [];
+
+textArray = text.split('');
+
+const howManyCharAndType = textArray.reduce((perv,cur) => {
+    const charType = cur;
+    let i = 1;
+    if (!perv[charType]){
+        perv[charType] = {charType: cur,numb: i};
+        
+    }else{
+         
+         perv[charType].numb = perv[charType].numb + 1;
+    }
+    return perv;
+},{})
+
+console.log(howManyCharAndType);
